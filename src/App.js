2d1a74727fc3421fea134b6327aeaeb00078e2fb
deramsey <ramseyd@clevelandcc.edu>
2023-03-09 16:45:46 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
+import Users from "./Users.js";
+
+import {
+  BrowserRouter as R,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+const Home = () =>{
+  return(
+    <h1>Hello World!</h1>
+  )
+}
 
 function App() {
   return (
+    <R>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Link to="/" style={{padding: 10}}>Home</Link>
+      <Link to="/users/1" style={{padding: 10}}>User 1</Link>
+      <Link to="/users/2" style={{padding: 10}}>User 2</Link>
+      <Link to="/users/3" style={{padding: 10}}>User 3</Link>
+      <Link to="/users/4" style={{padding: 10}}>User 4</Link>
+
+      
     </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/users/:uid" element={<Users />} />
+    </Routes>
+    </R>
   );
 }
 
